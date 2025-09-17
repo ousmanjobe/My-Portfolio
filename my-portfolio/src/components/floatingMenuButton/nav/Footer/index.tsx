c68739d4d608648b1acf8549af95efb3eb099724
Ousman Jobe <114046530+ousmanjobe@users.noreply.github.com>
2025-09-17 00:54:44 -0400

    Removed redundant font declarations in CSS files

--- my-portfolio/src/components/floatingMenuButton/nav/Footer/index.tsx
+++ my-portfolio/src/components/floatingMenuButton/nav/Footer/index.tsx
@@ -2,7 +2,7 @@ import styles from './style.module.scss';
 
 export default function Index() {
   return (
-    <div className={styles.footer}>
+    <div className={`${styles.footer} font-urbanist`}>
       <a href="mailto:jobe.o@northeastern.edu" target="_blank" rel="noopener noreferrer">EMAIL</a>
       <a href="https://www.linkedin.com/in/jobe-ousman" target="_blank" rel="noopener noreferrer">LINKEDIN</a>
       <a href="https://www.instagram.com/lounge_tales/profilecard/?igsh=NXl1cnl5M3RlaWRs" target="_blank" rel="noopener noreferrer">INSTAGRAM</a>
